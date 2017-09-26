c30742aabd30f50765379ae0c4e211a9d5c96a12
woodj22 <joe.wood@bbc.co.uk>
2017-09-26 22:03:08 +0100

    Add in if statement to map return and weighted tag integers

--- kingsley_scraper/scraper/reader.py
+++ kingsley_scraper/scraper/reader.py
@@ -18,13 +18,38 @@ def get_tag_strings(contents):
     return [(element.parent.name, element.split(' ')) for element in elements]
 
 
-def get_tag_to_singular_string(contents):
+def retrieve_tag_and_weights(contents):
     soup = BeautifulSoup(contents, "lxml")
     elements = soup.find_all(text=True)
 
+    # for element in elements:
+    #     if element.count(' '):
+    #         for word in element.split(' '):
+    #             return (element.parent.name, word)
+    #     else:
+    #         return (element, 5)
+    # [x+1 if x >= 45 else x+5 for x in l]
+
+    return [(word, tag_weight(element.parent.name)) if element.count(' ') else (element, tag_weight(element.parent.name)) for element in elements for word in element.split(' ')]
 
 
-    return [(element.parent.name, word) for element in elements for word in element.split(' ')]
+     # return [(element.parent.name, word) for element in elements for word in element.split(' ')]
+
+
+def tag_weight(tag):
+    # The weight of tag is out of 10
+    weight_list = {
+        'h1': 10,
+        'h2': 7,
+        'h3': 3,
+        'h4': 2,
+        'h5': 1,
+    }
+    default = 5
+    if tag in weight_list:
+        return weight_list.get(tag)
+    else:
+        return default
 
 
 def create_entry(text, tag_name):
--- kingsley_scraper/tests/scraper_reader_test.py
+++ kingsley_scraper/tests/scraper_reader_test.py
@@ -13,13 +13,13 @@ class ReaderUnitTest(unittest.TestCase):
 
         self.assertListEqual(expected_result, actual_result)
 
-    def test_get_tags_returns_an_array_of_strings_in_the_tag_when_their_are_two_of_the_same_tags_in_html(self):
-        sentance_string = "tiger lays low in the dirty forest"
-        sentance_string_1 = "Hello this is my best sentance."
-
-        content = html.unescape('<html> <h1>' + sentance_string + '</h1> <h1>' + sentance_string_1 + '</h1></html>')
-        actual_result = reader.get_tags(content, 'h1')
-        expected_result = sentance_string.split(' ')
+    def test_retrieve_tag_and_weight_returns_an_array_of_strings_in_the_tag_when_their_are_two_of_the_same_tags_in_html(self):
+        sentance_string = "tiger lays"
+        sentance_string_1 = "hello"
+        h1_weight = reader.tag_weight('h1')
+        content = html.unescape('<html><h1>' + sentance_string + '</h1><h1>' + sentance_string_1 + '</h1></html>')
+        actual_result = reader.retrieve_tag_and_weights(content)
+        expected_result = [('tiger', h1_weight), ('lays', h1_weight), ('hello', h1_weight)]
 
         self.assertListEqual(expected_result, actual_result)
 
@@ -32,12 +32,12 @@ class ReaderUnitTest(unittest.TestCase):
 
         self.assertListEqual(expected_result, actual_result)
 
-    def test_get_tag_singular_string_returns_a_list_of_tuples_containing_a_tag_string_pair(self):
+    def test_retrieve_tag_and_weight_returns_a_list_of_tuples_containing_a_weight_string_pair(self):
         sentance_string = "tiger lays"
         content = html.unescape("<html><h1>" + sentance_string + "<h3><h2>saf</h2></h3></h1></html>")
-        actual_result = reader.get_tag_to_singular_string(content)
-        expected_result = [('h1','tiger'),('h1', 'lays'), ('h2', ['saf'])]
+        actual_result = reader.retrieve_tag_and_weights(content)
 
+        expected_result = [('tiger', reader.tag_weight('h1')), ('lays', reader.tag_weight('h1')), ('saf', reader.tag_weight('h2'))]
         self.assertListEqual(expected_result, actual_result)
 
 
